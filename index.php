2d78b03ea38d00a1eb1585e882e17ccd0c004333
rfkymaulanaa <rifkym2605@gmail.com>
2025-03-10 13:05:59 +0800

    membuat file prodi.php

--- index.php
+++ index.php
@@ -7,5 +7,28 @@
 </head>
 <body>
     <h1>DATA MAHASISWA</h1>
+    <br>
+    <table border="1" cellspacing="0" cellpadding="5">
+        <thead>
+            <th>No</th>
+            <th>NIM</th>
+            <th>Nama</th>
+        </thead>
+
+        <tbody>
+        <tr>
+            <td>1</td>
+            <td>E020323057</td>
+            <td>anang</td>
+        </tr>
+            <br>
+        <tr>
+            <td>2</td>
+            <td>E020323077</td>
+            <td>irfan</td>
+        </tr>
+
+        </tbody>
+    </table>
 </body>
 </html>
